cd37342d262c6a0a7ea73237eccba61429dde5ee
Mariel Duran <marielduran84@gmail.com>
2023-01-02 08:05:36 +0200

    day4

--- day4.py
+++ day4.py
@@ -9,6 +9,17 @@ sample = """2-4,6-8
 def assignment_checker(assignment_string:str):
     assignment_list = [x.split(",") for x in assignment_string.splitlines()]
     print(assignment_list)
+    overlapping = 0
+    for list in assignment_list:
+        assignment1 = [int(x) for x in list[0].split("-")]
+        range1 = range(assignment1[0], assignment1[1]+1)
+        print(range1)
+        assignment2 = [int(x) for x in list[1].split("-")]
+        range2 = range(assignment2[0], assignment2[1]+1)
+        for area in range1:
+            if area not in list(range2):
+                print("hola")
+
 
 
 
